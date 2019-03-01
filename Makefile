07f477381b9f563932d05677edfacd581c06cc4d
panjiang <panjiang@mobartsgame.com>
2019-03-01 12:06:20 +0800

    build for all platform os

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+build:
+	bash ./sh/build-all.sh
---
+++ sh/build-all.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+APP="redisbench"
+PLATFORMS="darwin freebsd linux windows"
+ARCHS="amd64 386"
+
+for OS in ${PLATFORMS[@]}; do 
+    for ARCH in ${ARCHS[@]}; do 
+        NAME="${APP}-${OS}-${ARCH}"
+        if [[ "${OS}" == "windows" ]]; then
+            NAME="${NAME}.exe"
+        fi
+
+        env GOOS=${OS} GOARCH=${ARCH} go build -o release/${NAME} .
+    done
+done
